47b31e8b0f78bfa0762983ba9f3b99fa582a86a6
elly <elly.chan.mda@gmail.com>
2018-10-30 16:15:15 +0000

    added dock_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,13 @@ describe DockingStation do
   it 'gives us a working bike' do
     expect(subject.release_bike.working?).to eq true
   end
+
+	it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+	it "return docked bikes" do
+		bike = Bike.new
+		subject.dock_bike(bike)
+		expect(subject.bike).to eq bike
+	end
+
 end
